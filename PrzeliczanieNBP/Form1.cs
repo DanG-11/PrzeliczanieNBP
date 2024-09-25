665043d4a8ea54275692cfc919722f2d5ca5bc47
Daniil Gavrilov <daniil.gavrilov@teb.edu.pl>
2024-09-25 10:40:35 +0200

    Dodane kursy walut

--- PrzeliczanieNBP/Form1.cs
+++ PrzeliczanieNBP/Form1.cs
@@ -4,13 +4,17 @@ namespace PrzeliczanieNBP
 {
     public partial class FormPrzelicznikWalut : Form
     {
+        Dictionary<string, decimal> exchangeRates = new Dictionary<string, decimal> { };
 
         public FormPrzelicznikWalut()
         {
             InitializeComponent();
             NumBoxWalutaDoKalk.Controls.RemoveAt(0);
             NumBoxWalutaPoKalk.Controls.RemoveAt(0);
-            
+            exchangeRates.Add("Dolar", Convert.ToDecimal(0.2626));
+            exchangeRates.Add("Euro", Convert.ToDecimal(0.2347));
+            exchangeRates.Add("Rubel", Convert.ToDecimal(24.3931));
+            exchangeRates.Add("Bat", Convert.ToDecimal(8.5834));
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
@@ -20,30 +24,24 @@ namespace PrzeliczanieNBP
 
         private void BtnKalkuluj_Click(object sender, EventArgs e)
         {
-
-            decimal Dolar = Convert.ToDecimal(0.26);
-            decimal Euro = Convert.ToDecimal(0.23);
-            decimal Rubel = Convert.ToDecimal(24.38);
-            decimal Bat = Convert.ToDecimal(8.58);
-
             if (RadBtnToUSD.Checked)
             {
-                NumBoxWalutaPoKalk.Value = NumBoxWalutaDoKalk.Value * Dolar;
+                NumBoxWalutaPoKalk.Value = NumBoxWalutaDoKalk.Value * exchangeRates["Dolar"];
                 LblWalutaPoKalk.Text = "Dolar";
             }
             else if (RadBtnToEUR.Checked)
             {
-                NumBoxWalutaPoKalk.Value = NumBoxWalutaDoKalk.Value * Euro;
+                NumBoxWalutaPoKalk.Value = NumBoxWalutaDoKalk.Value * exchangeRates["Euro"];
                 LblWalutaPoKalk.Text = "Euro";
             }
             else if (RadBtnToRUB.Checked)
             {
-                NumBoxWalutaPoKalk.Value = NumBoxWalutaDoKalk.Value * Rubel;
+                NumBoxWalutaPoKalk.Value = NumBoxWalutaDoKalk.Value * exchangeRates["Rubel"];
                 LblWalutaPoKalk.Text = "Rubel";
             }
             else if (RadBtnToBAT.Checked)
             {
-                NumBoxWalutaPoKalk.Value = NumBoxWalutaDoKalk.Value * Bat;
+                NumBoxWalutaPoKalk.Value = NumBoxWalutaDoKalk.Value * exchangeRates["Bat"];
                 LblWalutaPoKalk.Text = "Bat";
             }
         }
