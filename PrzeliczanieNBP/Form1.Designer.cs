44b4648d09674d888983696c569d688ab311dd8a
Daniil Gavrilov <daniil.gavrilov@teb.edu.pl>
2024-09-27 09:41:49 +0200

    Dodano kalkulacje do pln, oraz zmiane labela po kliknięciu radio buttona.

--- PrzeliczanieNBP/Form1.Designer.cs
+++ PrzeliczanieNBP/Form1.Designer.cs
@@ -33,11 +33,12 @@
             RadBtnToEUR = new RadioButton();
             RadBtnToRUB = new RadioButton();
             RadBtnToBAT = new RadioButton();
-            BtnKalkuluj = new Button();
+            BtnKalkulujZPLN = new Button();
             LblWalutaPoKalk = new Label();
             GrpBoxToWaluta = new GroupBox();
             NumBoxWalutaDoKalk = new NumericUpDown();
             NumBoxWalutaPoKalk = new NumericUpDown();
+            BtnKalkulujDoPLN = new Button();
             GrpBoxToWaluta.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)NumBoxWalutaDoKalk).BeginInit();
             ((System.ComponentModel.ISupportInitialize)NumBoxWalutaPoKalk).BeginInit();
@@ -57,59 +58,63 @@
             RadBtnToUSD.AutoSize = true;
             RadBtnToUSD.Location = new Point(6, 15);
             RadBtnToUSD.Name = "RadBtnToUSD";
-            RadBtnToUSD.Size = new Size(61, 19);
+            RadBtnToUSD.Size = new Size(92, 19);
             RadBtnToUSD.TabIndex = 0;
             RadBtnToUSD.TabStop = true;
-            RadBtnToUSD.Text = "to USD";
+            RadBtnToUSD.Text = "to/from USD";
             RadBtnToUSD.UseVisualStyleBackColor = true;
+            RadBtnToUSD.CheckedChanged += RadBtnToUSD_CheckedChanged;
             // 
             // RadBtnToEUR
             // 
             RadBtnToEUR.AutoSize = true;
-            RadBtnToEUR.Location = new Point(73, 15);
+            RadBtnToEUR.Location = new Point(104, 15);
             RadBtnToEUR.Name = "RadBtnToEUR";
-            RadBtnToEUR.Size = new Size(60, 19);
+            RadBtnToEUR.Size = new Size(91, 19);
             RadBtnToEUR.TabIndex = 1;
             RadBtnToEUR.TabStop = true;
-            RadBtnToEUR.Text = "to EUR";
+            RadBtnToEUR.Text = "to/from EUR";
             RadBtnToEUR.UseVisualStyleBackColor = true;
+            RadBtnToEUR.CheckedChanged += RadBtnToEUR_CheckedChanged;
             // 
             // RadBtnToRUB
             // 
             RadBtnToRUB.AutoSize = true;
-            RadBtnToRUB.Location = new Point(139, 15);
+            RadBtnToRUB.Location = new Point(6, 40);
             RadBtnToRUB.Name = "RadBtnToRUB";
-            RadBtnToRUB.Size = new Size(61, 19);
+            RadBtnToRUB.Size = new Size(92, 19);
             RadBtnToRUB.TabIndex = 2;
             RadBtnToRUB.TabStop = true;
-            RadBtnToRUB.Text = "to RUB";
+            RadBtnToRUB.Text = "to/from RUB";
             RadBtnToRUB.UseVisualStyleBackColor = true;
+            RadBtnToRUB.CheckedChanged += RadBtnToRUB_CheckedChanged;
             // 
             // RadBtnToBAT
             // 
             RadBtnToBAT.AutoSize = true;
-            RadBtnToBAT.Location = new Point(206, 15);
+            RadBtnToBAT.Location = new Point(104, 40);
             RadBtnToBAT.Name = "RadBtnToBAT";
-            RadBtnToBAT.Size = new Size(59, 19);
+            RadBtnToBAT.Size = new Size(90, 19);
             RadBtnToBAT.TabIndex = 3;
             RadBtnToBAT.TabStop = true;
-            RadBtnToBAT.Text = "to BAT";
+            RadBtnToBAT.Text = "to/from BAT";
             RadBtnToBAT.UseVisualStyleBackColor = true;
+            RadBtnToBAT.CheckedChanged += RadBtnToBAT_CheckedChanged;
             // 
-            // BtnKalkuluj
+            // BtnKalkulujZPLN
             // 
-            BtnKalkuluj.Location = new Point(12, 56);
-            BtnKalkuluj.Name = "BtnKalkuluj";
-            BtnKalkuluj.Size = new Size(480, 23);
-            BtnKalkuluj.TabIndex = 5;
-            BtnKalkuluj.Text = "Kalkuluj";
-            BtnKalkuluj.UseVisualStyleBackColor = true;
-            BtnKalkuluj.Click += BtnKalkuluj_Click;
+            BtnKalkulujZPLN.Location = new Point(12, 83);
+            BtnKalkulujZPLN.Name = "BtnKalkulujZPLN";
+            BtnKalkulujZPLN.Size = new Size(204, 23);
+            BtnKalkulujZPLN.TabIndex = 5;
+            BtnKalkulujZPLN.Text = "Kalkuluj z PLN";
+            BtnKalkulujZPLN.UseVisualStyleBackColor = true;
+            BtnKalkulujZPLN.Click += BtnKalkuluj_Click;
             // 
             // LblWalutaPoKalk
             // 
             LblWalutaPoKalk.AutoSize = true;
-            LblWalutaPoKalk.Location = new Point(423, 9);
+            LblWalutaPoKalk.Location = new Point(342, 9);
             LblWalutaPoKalk.Name = "LblWalutaPoKalk";
             LblWalutaPoKalk.Size = new Size(0, 15);
             LblWalutaPoKalk.TabIndex = 1;
@@ -118,11 +123,11 @@
             // 
             GrpBoxToWaluta.Controls.Add(RadBtnToUSD);
             GrpBoxToWaluta.Controls.Add(RadBtnToEUR);
-            GrpBoxToWaluta.Controls.Add(RadBtnToRUB);
             GrpBoxToWaluta.Controls.Add(RadBtnToBAT);
+            GrpBoxToWaluta.Controls.Add(RadBtnToRUB);
             GrpBoxToWaluta.Location = new Point(118, 12);
             GrpBoxToWaluta.Name = "GrpBoxToWaluta";
-            GrpBoxToWaluta.Size = new Size(268, 38);
+            GrpBoxToWaluta.Size = new Size(201, 65);
             GrpBoxToWaluta.TabIndex = 3;
             GrpBoxToWaluta.TabStop = false;
             GrpBoxToWaluta.Text = "Typ kalkulacji";
@@ -132,6 +137,7 @@
             NumBoxWalutaDoKalk.DecimalPlaces = 2;
             NumBoxWalutaDoKalk.Increment = new decimal(new int[] { 1, 0, 0, 131072 });
             NumBoxWalutaDoKalk.Location = new Point(12, 23);
+            NumBoxWalutaDoKalk.Maximum = new decimal(new int[] { -1981284352, -1966660860, 0, 0 });
             NumBoxWalutaDoKalk.Name = "NumBoxWalutaDoKalk";
             NumBoxWalutaDoKalk.Size = new Size(100, 23);
             NumBoxWalutaDoKalk.TabIndex = 2;
@@ -140,21 +146,33 @@
             // 
             NumBoxWalutaPoKalk.DecimalPlaces = 2;
             NumBoxWalutaPoKalk.Increment = new decimal(new int[] { 1, 0, 0, 131072 });
-            NumBoxWalutaPoKalk.Location = new Point(392, 23);
+            NumBoxWalutaPoKalk.Location = new Point(325, 23);
+            NumBoxWalutaPoKalk.Maximum = new decimal(new int[] { -159383552, 46653770, 5421, 0 });
             NumBoxWalutaPoKalk.Name = "NumBoxWalutaPoKalk";
-            NumBoxWalutaPoKalk.Size = new Size(100, 23);
+            NumBoxWalutaPoKalk.Size = new Size(102, 23);
             NumBoxWalutaPoKalk.TabIndex = 4;
+            // 
+            // BtnKalkulujDoPLN
+            // 
+            BtnKalkulujDoPLN.Location = new Point(222, 83);
+            BtnKalkulujDoPLN.Name = "BtnKalkulujDoPLN";
+            BtnKalkulujDoPLN.Size = new Size(205, 23);
+            BtnKalkulujDoPLN.TabIndex = 6;
+            BtnKalkulujDoPLN.Text = "Kalkuluj do PLN";
+            BtnKalkulujDoPLN.UseVisualStyleBackColor = true;
+            BtnKalkulujDoPLN.Click += BtnKalkulujDoPLN_Click;
             // 
             // FormPrzelicznikWalut
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(506, 90);
+            ClientSize = new Size(436, 118);
+            Controls.Add(BtnKalkulujDoPLN);
             Controls.Add(NumBoxWalutaPoKalk);
             Controls.Add(NumBoxWalutaDoKalk);
             Controls.Add(GrpBoxToWaluta);
             Controls.Add(LblWalutaPoKalk);
-            Controls.Add(BtnKalkuluj);
+            Controls.Add(BtnKalkulujZPLN);
             Controls.Add(LblWalutaDoKalk);
             Name = "FormPrzelicznikWalut";
             Text = "PrzelicznikWalut";
@@ -172,10 +190,11 @@
         private RadioButton RadBtnToEUR;
         private RadioButton RadBtnToRUB;
         private RadioButton RadBtnToBAT;
-        private Button BtnKalkuluj;
+        private Button BtnKalkulujZPLN;
         private Label LblWalutaPoKalk;
         private GroupBox GrpBoxToWaluta;
         private NumericUpDown NumBoxWalutaDoKalk;
         private NumericUpDown NumBoxWalutaPoKalk;
+        private Button BtnKalkulujDoPLN;
     }
 }
--- PrzeliczanieNBP/Form1.cs
+++ PrzeliczanieNBP/Form1.cs
@@ -4,17 +4,25 @@ namespace PrzeliczanieNBP
 {
     public partial class FormPrzelicznikWalut : Form
     {
-        Dictionary<string, decimal> exchangeRates = new Dictionary<string, decimal> { };
+        Dictionary<string, decimal> WartoœæWalutyZPLN = new Dictionary<string, decimal> { };
+        Dictionary<string, decimal> WartoœæWalutyDoPLN = new Dictionary<string, decimal> { };
 
         public FormPrzelicznikWalut()
         {
             InitializeComponent();
+
             NumBoxWalutaDoKalk.Controls.RemoveAt(0);
             NumBoxWalutaPoKalk.Controls.RemoveAt(0);
-            exchangeRates.Add("Dolar", Convert.ToDecimal(0.2626));
-            exchangeRates.Add("Euro", Convert.ToDecimal(0.2347));
-            exchangeRates.Add("Rubel", Convert.ToDecimal(24.3931));
-            exchangeRates.Add("Bat", Convert.ToDecimal(8.5834));
+
+            WartoœæWalutyZPLN.Add("Dolar", Convert.ToDecimal(0.2626));
+            WartoœæWalutyZPLN.Add("Euro", Convert.ToDecimal(0.2347));
+            WartoœæWalutyZPLN.Add("Rubel", Convert.ToDecimal(24.3931));
+            WartoœæWalutyZPLN.Add("Bat", Convert.ToDecimal(8.5834));
+
+            WartoœæWalutyDoPLN.Add("Dolar", Convert.ToDecimal(3.8415));
+            WartoœæWalutyDoPLN.Add("Euro", Convert.ToDecimal(4.2763));
+            WartoœæWalutyDoPLN.Add("Rubel", Convert.ToDecimal(0.04138));
+            WartoœæWalutyDoPLN.Add("Bat", Convert.ToDecimal(0.1183));
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
@@ -26,23 +34,59 @@ namespace PrzeliczanieNBP
         {
             if (RadBtnToUSD.Checked)
             {
-                NumBoxWalutaPoKalk.Value = NumBoxWalutaDoKalk.Value * exchangeRates["Dolar"];
-                LblWalutaPoKalk.Text = "Dolar";
+                NumBoxWalutaPoKalk.Value = NumBoxWalutaDoKalk.Value * WartoœæWalutyZPLN["Dolar"];
             }
             else if (RadBtnToEUR.Checked)
             {
-                NumBoxWalutaPoKalk.Value = NumBoxWalutaDoKalk.Value * exchangeRates["Euro"];
-                LblWalutaPoKalk.Text = "Euro";
+                NumBoxWalutaPoKalk.Value = NumBoxWalutaDoKalk.Value * WartoœæWalutyZPLN["Euro"];
             }
             else if (RadBtnToRUB.Checked)
             {
-                NumBoxWalutaPoKalk.Value = NumBoxWalutaDoKalk.Value * exchangeRates["Rubel"];
-                LblWalutaPoKalk.Text = "Rubel";
+                NumBoxWalutaPoKalk.Value = NumBoxWalutaDoKalk.Value * WartoœæWalutyZPLN["Rubel"];
             }
             else if (RadBtnToBAT.Checked)
             {
-                NumBoxWalutaPoKalk.Value = NumBoxWalutaDoKalk.Value * exchangeRates["Bat"];
-                LblWalutaPoKalk.Text = "Bat";
+                NumBoxWalutaPoKalk.Value = NumBoxWalutaDoKalk.Value * WartoœæWalutyZPLN["Bat"];
+            }
+        }
+
+        private void RadBtnToUSD_CheckedChanged(object sender, EventArgs e)
+        {
+            LblWalutaPoKalk.Text = "Dolar";
+        }
+
+        private void RadBtnToEUR_CheckedChanged(object sender, EventArgs e)
+        {
+            LblWalutaPoKalk.Text = "Euro";
+        }
+
+        private void RadBtnToRUB_CheckedChanged(object sender, EventArgs e)
+        {
+            LblWalutaPoKalk.Text = "Rubel";
+        }
+
+        private void RadBtnToBAT_CheckedChanged(object sender, EventArgs e)
+        {
+            LblWalutaPoKalk.Text = "Bat";
+        }
+
+        private void BtnKalkulujDoPLN_Click(object sender, EventArgs e)
+        {
+            if (RadBtnToUSD.Checked)
+            {
+                NumBoxWalutaDoKalk.Value = NumBoxWalutaPoKalk.Value * WartoœæWalutyDoPLN["Dolar"];
+            }
+            else if (RadBtnToEUR.Checked)
+            {
+                NumBoxWalutaDoKalk.Value = NumBoxWalutaPoKalk.Value * WartoœæWalutyDoPLN["Euro"];
+            }
+            else if (RadBtnToRUB.Checked)
+            {
+                NumBoxWalutaDoKalk.Value = NumBoxWalutaPoKalk.Value * WartoœæWalutyDoPLN["Rubel"];
+            }
+            else if (RadBtnToBAT.Checked)
+            {
+                NumBoxWalutaDoKalk.Value = NumBoxWalutaPoKalk.Value * WartoœæWalutyDoPLN["Bat"];
             }
         }
     }
